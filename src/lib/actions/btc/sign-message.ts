6c723d4f19b51a20989c9b212cac737870a44f6b
AricRedemption <aricredemption@gmail.com>
2024-04-26 18:43:50 +0800

    fix: sign message api

--- src/lib/actions/btc/sign-message.ts
+++ src/lib/actions/btc/sign-message.ts
@@ -1,7 +1,7 @@
 import { getCurrentWallet } from '@/lib/wallet'
 import { Chain } from '@metalet/utxo-wallet-service'
 
-export async function process(message: string): Promise<string> {
+export async function process(message: string, encoding?: BufferEncoding): Promise<string> {
   const wallet = await getCurrentWallet(Chain.BTC)
-  return wallet.signMessage(message)
+  return wallet.signMessage(message, encoding)
 }
--- src/lib/actions/sign-message.ts
+++ src/lib/actions/sign-message.ts
@@ -1,8 +1,12 @@
+import { mvc } from 'meta-contract'
+import { signMessage } from '../crypto'
 import { getCurrentWallet } from '../wallet'
 import { Chain } from '@metalet/utxo-wallet-service'
 
-export async function process(params: any, host: string) {
+export async function process(params: any) {
   const wallet = await getCurrentWallet(Chain.MVC)
-  const signature = wallet.signMessage(params.message)
-  return { signature: { signature } }
+  const wif = wallet.getPrivateKey()
+  const privateKeyObj = mvc.PrivateKey.fromWIF(wif)
+  const signature = signMessage(params.message, privateKeyObj, params.encoding)
+  return { signature }
 }
