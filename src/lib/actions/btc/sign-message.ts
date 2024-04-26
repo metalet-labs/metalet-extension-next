45adc27df53fdee97e86c88e9d6ecc73f67e875d
AricRedemption <aricredemption@gmail.com>
2024-04-26 19:19:59 +0800

    fix: btc sign message again

--- src/lib/actions/btc/sign-message.ts
+++ src/lib/actions/btc/sign-message.ts
@@ -1,7 +1,10 @@
 import { getCurrentWallet } from '@/lib/wallet'
+import { Message, PrivateKey } from 'bitcore-lib'
 import { Chain } from '@metalet/utxo-wallet-service'
 
 export async function process(message: string, encoding?: BufferEncoding): Promise<string> {
   const wallet = await getCurrentWallet(Chain.BTC)
-  return wallet.signMessage(message, encoding ?? 'base64')
+  const privateKey = wallet.getPrivateKey()
+  const signMessage = new Message(message)
+  return signMessage.sign(new PrivateKey(privateKey))
 }
