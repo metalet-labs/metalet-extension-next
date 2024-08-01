af7c054c9b72a858601aaa183f1388c3062a8b83
AricRedemption <aricredemption@gmail.com>
2024-08-01 18:03:16 +0800

    chore: add btc transfer api

--- src/lib/actions/btc/transfer.ts
+++ src/lib/actions/btc/transfer.ts
@@ -1,3 +1,47 @@
-export async function process() {
-  throw new Error('The method is no longer supported')
+import Decimal from 'decimal.js'
+import { addSafeUtxo } from '@/lib/utxo'
+import { getBtcUtxos } from '@/queries/utxos'
+import { getCurrentWallet } from '@/lib/wallet'
+import { broadcastBTCTx, getBTCTRate } from '@/queries/transaction'
+import { Chain, ScriptType, SignType, Transaction, getAddressFromScript } from '@metalet/utxo-wallet-service'
+
+interface TransferParameters {
+  toAddress: string
+  satoshis: string
+  options?: { noBroadcast?: boolean; feeRate?: string | number }
+}
+
+export async function process(params: TransferParameters): Promise<{ txId: string } | { txHex: string }> {
+  const wallet = await getCurrentWallet(Chain.BTC)
+  const address = wallet.getAddress()
+
+  let feeRate = params.options?.feeRate
+
+  if (!feeRate) {
+    const { list } = await getBTCTRate()
+    feeRate = list.find((item) => item.title === 'Avg')?.feeRate
+    throw Error('Request feeRate Error')
+  }
+
+  feeRate = Number(feeRate)
+
+  const utxos = await getBtcUtxos(wallet.getAddress(), wallet.getScriptType() === ScriptType.P2PKH, true)
+
+  const { rawTx } = wallet.signTx(SignType.SEND, {
+    recipient: params.toAddress,
+    amount: new Decimal(params.satoshis).toNumber(),
+    feeRate,
+    utxos,
+  })
+
+  if (params.options?.noBroadcast) {
+    return { txHex: rawTx }
+  }
+
+  const txId = await broadcastBTCTx(rawTx)
+  const tx = Transaction.fromHex(rawTx)
+  if (tx.outs.length > 1 && getAddressFromScript(tx.outs[tx.outs.length - 1].script, wallet.getNetwork()) === address) {
+    await addSafeUtxo(address, `${txId}:${tx.outs.length - 1}`)
+  }
+  return { txId }
 }
