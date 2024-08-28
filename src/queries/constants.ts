592d88da42757dc60d7f7628f83f50395105c9ff
AricRedemption <aricredemption@gmail.com>
2024-08-29 00:08:02 +0800

    chore: update refresh time

--- src/queries/constants.ts
+++ src/queries/constants.ts
@@ -1,3 +1,3 @@
-export const COMMON_INTERVAL = 30000
-export const Balance_QUERY_INTERVAL = 30000
-export const Activities_QUERY_INTERVAL = 30000
+export const COMMON_INTERVAL = 10000
+export const Balance_QUERY_INTERVAL = 10000
+export const Activities_QUERY_INTERVAL = 10000
