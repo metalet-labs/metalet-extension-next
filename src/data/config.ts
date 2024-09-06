961c472c9adde17aa55590f12bbf1b43e0c19ae6
AricRedemption <aricredemption@gmail.com>
2024-09-06 09:07:47 +0800

    chore: close dev mode

--- src/data/config.ts
+++ src/data/config.ts
@@ -9,7 +9,7 @@ const IS_WINDOWS = /windows/i.test(navigator.userAgent)
 export const NOTIFICATION_WIDTH = IS_WINDOWS ? 366 : 360
 export const NOTIFICATION_HEIGHT = IS_WINDOWS ? 640 : 630
 
-export const DEBUG = true
+export const DEBUG = false
 export const IS_DEV = process.env.NODE_ENV === 'development'
 
 export const UNISAT_ENABLED = false
