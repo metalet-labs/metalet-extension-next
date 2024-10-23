b55ec0dcd721bf25b4ce4585c285addb7d7fcf1a
AricRedemption <aricredemption@gmail.com>
2024-10-23 17:27:27 +0800

    chore: update test UI

--- src/pages/tests/Index.vue
+++ src/pages/tests/Index.vue
@@ -1,29 +1,46 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { getPassword } from '@/lib/lock'
 import { process } from '@/lib/actions/common/ecdh'
+
+const result = ref()
+const inputValue = ref(
+  '040227de44169fc5faed3e03a300c9cf800e9a88e716c3284be484d720ffcc21e60283728dbf422b3865a8ee3779db46a4a3685aa87ed53512cf7c77e288267235'
+)
+
+const handleClick = async () => {
+  const password = await getPassword()
+  const data = await process(
+    {
+      externalPubKey: inputValue.value,
+    },
+    { password }
+  )
+  result.value = data
+  console.log('data', data)
+}
 </script>
 
 <template>
-  <div class="w-full py-2 flex items-center justify-center">
-    <button
-      class="text-lg"
-      @click="
-        async () => {
-          const password = await getPassword()
-          const data = await process(
-            {
-              externalPubKey:
-                '040227de44169fc5faed3e03a300c9cf800e9a88e716c3284be484d720ffcc21e60283728dbf422b3865a8ee3779db46a4a3685aa87ed53512cf7c77e288267235',
-            },
-            { password }
-          )
-          console.log('data', data)
-        }
-      "
-    >
-      test
-    </button>
+  <div class="w-full py-2 flex flex-col items-center justify-center">
+    <textarea v-model="inputValue" type="text" class="my-16 p-2 border border-gray-300 rounded w-[600px]" rows="4" placeholder="输入内容" />
+    <button class="text-lg mb-4 p-2 bg-blue-500 text-white rounded" @click="handleClick">Get Data</button>
+    <div v-if="result" class="p-4 border border-gray-300 rounded w-[600px]">
+      <p>
+        <strong>sharedSecret:</strong>
+        {{ result.sharedSecret }}
+      </p>
+      <p>
+        <strong>privateKey:</strong>
+        {{ result.privateKey }}
+      </p>
+      <p class="break-all">
+        <strong>externalPubKey:</strong>
+        {{ result.externalPubKey }}
+      </p>
+    </div>
   </div>
 </template>
 
-<style scoped lang="css"></style>
+<style scoped lang="css">
+</style>
