932fea7d8a9b818cd5ec48409d652a687b446679
AricRedemption <aricredemption@gmail.com>
2024-12-16 18:01:51 +0800

    📦 VERSION: 3.3.12

--- src/lib/actions/preview-transaction.ts
+++ src/lib/actions/preview-transaction.ts
@@ -1,7 +1,7 @@
 import { signTransaction } from '../crypto'
 
 export async function process(params: any, options?: { password: string }) {
-  console.log('options', options)
+  // console.log('options', options)
 
   const { txid } = await signTransaction(params.transaction, true, options)
   return { txid }
