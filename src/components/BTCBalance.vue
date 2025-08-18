0b90b1b3bdb7b4bac74ca37d14e6d43541416d36
feiyang1020 <feiyangl1020@163.com>
2025-08-18 19:46:04 +0800

    styles

--- src/components/BTCBalance.vue
+++ src/components/BTCBalance.vue
@@ -16,14 +16,14 @@ const showTooltip = ref(false)
 
 <template>
   <div
-    class="flex items-center justify-between w-full text-sm leading-5 text-primary bg-[#f5f5f5] rounded-full p-3 font-sm relative">
-    <div class="flex items-center gap-1">
-      <span class="font-sm text-[#999]">{{ $t('Common.Avail') }} <span class="text-[#303133]">{{ new Decimal(safeBalance
+    class="flex items-center justify-between w-full text-xs leading-5 text-primary bg-[#f5f5f5] rounded-full p-3  relative">
+    <div class="flex items-center gap-1 text-nowrap">
+      <span class=" text-[#999] text-nowrap">{{ $t('Common.Avail') }} <span class="text-[#303133]">{{ new Decimal(safeBalance
         ||
         0).div(10 ** 8).toFixed(8) }} BTC</span>
       </span>
-      <span class="border-l border-gray-300 h-4 mx-2"></span>
-      <span class="text-[#999]">
+      <span class="border-l border-gray-300 h-4 mx-1"></span>
+      <span class="text-[#999] text-nowrap">
         N/A <span class="text-[#303133]">{{ new Decimal((balance - safeBalance) || 0).div(10 ** 8).toFixed(8) }}
           BTC</span>
       </span>
@@ -33,14 +33,20 @@ const showTooltip = ref(false)
       @click.stop="showTooltip = !showTooltip">
       ?
     </button>
-    <div v-if="showTooltip" @click.stop="showTooltip = !showTooltip"
-      class="absolute top-full mt-2 p-3 text-xs text-[#676767] bg-white  rounded-xl  z-10 flex align-top gap-2 border border-[#eee]" >
-      <button
-        class="w-3 h-3 min-h-3 min-w-3 flex items-center justify-center text-[#676767] hover:text-[#676767] transition-colors rounded-full border border-[#676767]">
-        ?
-      </button>
-      {{ $t('Common.BTCBalanceTooltip') }}
-    </div>
+    <Teleport to="body">
+      <div v-if="showTooltip" class="fixed inset-0 z-[9999] flex items-center justify-center">
+        <div class="absolute inset-0 bg-black/20" @click="showTooltip = false"></div>
+        <div class="relative bg-white rounded-xl p-4 max-w-xs mx-4 shadow-lg">
+          <div class="flex items-start gap-2">
+            <button
+              class="w-4 h-4 flex-shrink-0 flex items-center justify-center text-[#676767] rounded-full border border-[#676767]">
+              ?
+            </button>
+            <p class="text-sm text-[#676767]">{{ $t('Common.BTCBalanceTooltip') }}</p>
+          </div>
+        </div>
+      </div>
+    </Teleport>
   </div>
 </template>
 
